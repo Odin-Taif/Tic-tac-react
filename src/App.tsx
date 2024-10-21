6a551591e0e4bb60be1ee48d37ee9724eb4ff638
Odin Thif <mjd.thif@icloud.com>
2024-10-21 12:29:20 +0200

    Taking turns  | Declaring a winner

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,55 @@
 import { useState } from "react";
 import { ButtonX } from "./components";
+import { calculateWinner } from "./lib/functionalcore";
 
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
-  function handleClick(i: number) {
-    console.log(i);
-    const nextSquares = squares.slice(); // with slice we are create a shallow copy of squares array.
-    nextSquares[i] = "X";
-    setSquares(nextSquares);
+  const winner = calculateWinner(squares);
+
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
   }
+
+  function handleClick(i: number) {
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
+    const nextSquares = squares.slice(); // with slice we are create a shallow copy of squares array.
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
+
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-red-100">
-      <div className="">
-        <ButtonX value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <ButtonX value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <ButtonX value={squares[2]} onSquareClick={() => handleClick(2)} />
+    <>
+      <div className="flex flex-col items-center justify-center h-screen bg-red-100">
+        <div className="">
+          <ButtonX value={squares[0]} onSquareClick={() => handleClick(0)} />
+          <ButtonX value={squares[1]} onSquareClick={() => handleClick(1)} />
+          <ButtonX value={squares[2]} onSquareClick={() => handleClick(2)} />
+        </div>
+        <div className="">
+          <ButtonX value={squares[3]} onSquareClick={() => handleClick(3)} />
+          <ButtonX value={squares[4]} onSquareClick={() => handleClick(4)} />
+          <ButtonX value={squares[5]} onSquareClick={() => handleClick(5)} />
+        </div>
+        <div className="">
+          <ButtonX value={squares[6]} onSquareClick={() => handleClick(6)} />
+          <ButtonX value={squares[7]} onSquareClick={() => handleClick(7)} />
+          <ButtonX value={squares[8]} onSquareClick={() => handleClick(8)} />
+        </div>
+        <div className="bg-green-800 p-5 text-lg flex items-center justify-center w-full">
+          <span className="text-xl text-white">{status}</span>
+        </div>
       </div>
-      <div className="">
-        <ButtonX value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <ButtonX value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <ButtonX value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="">
-        <ButtonX value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <ButtonX value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <ButtonX value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
-    </div>
+    </>
   );
 }
